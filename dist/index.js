240c358a7e7b7d0e13165bdf0c760a6124a744f2
Karishma <karishma.7022@gmail.com>
2026-07-03 17:03:44 +0530

    initial setup + user interface

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sumOfAge(user1, user2) {
+    return user1.age + user2.age;
+}
+let user1 = {
+    id: 1,
+    name: "Karishma",
+    email: "karishma.7022@gmail.com",
+    age: 25
+};
+let user2 = {
+    id: 2,
+    name: "Priya",
+    email: "priya.7022@gmail.com",
+    age: 24
+};
+console.log(sumOfAge(user1, user2));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+interface User{
+    id:number,
+    name:string,
+    email:string,
+    age:number
+}
+
+function sumOfAge(user1: User,user2:User):number{
+    return user1.age + user2.age
+}
+
+let user1: User={
+    id:1,
+    name:"Karishma",
+    email:"karishma.7022@gmail.com",
+    age:25
+}
+
+let user2: User={
+    id:2,
+    name:"Priya",
+    email:"priya.7022@gmail.com",
+    age:24
+}
+
+console.log(sumOfAge(user1,user2));
